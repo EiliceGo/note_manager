c021309fd4730ae8ba4d37142f58b961d09462e5
EiliceGo <aligorch1997@gmail.com>
2024-12-22 22:20:21 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,4 @@
+created_date = input ('Enter the date of creation in the format dd-mm-yy:')
+print('Date of creation: ', created_date[:-3])
+issue_date = input('Enter deadline in format dd-mm-yy:')
+print('Deadline: ', issue_date[:-3])
