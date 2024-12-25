a3413a518d3c5901c936846352c6e8f63ef889e2
EiliceGo <aligorch1997@gmail.com>
2024-12-25 13:25:11 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+#Данные в переменных
+username = 'Alisa'
+title = 'My study'
+content = 'Info about my progress'
+status = 'Active'
+created_date = '21-12-24'
+issue_date = '22-12-24'
+#Вывод данных
+print('Name:', username)
+print('Title name:', title)
+print('Description:', content)
+print('Status:', status)
+print('Date of creation: ', created_date)
+print('Deadline: ', issue_date)
